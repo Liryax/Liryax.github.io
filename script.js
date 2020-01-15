90fadc50cdaf4938d07e52463d1b06d14179646a
Liryax <jo43pml@hotmail.fr>
2020-01-15 21:17:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("lorem ipsum toggle").addEventListener("change", function(e){
-	document.getElementById("lorem ipsum").hidden = (this.checked == false)
-})
-
-// document.getElementById("footer").style.position = "static"
-// document.getElementById("footer").style.transform = ""
-
-// document.getElementById("footer").style.position = "fixed"
-// document.getElementById("footer").style.transform = "translate(-50%,0)"
